2023a62a1a5d7a5acf130f52157f14de3ecd9c03
Adrian <adriansacloloramirez@gmail.com>
2026-09-21 01:05:47 +0800

    input  manager setup

---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,36 @@
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    [ReadOnly] public Vector2 movementInput;
+    [ReadOnly] public Vector2 pointerInput;
+    private PlayerControls playerControls;
+
+    private void Awake()
+    {
+        playerControls = new();
+
+        playerControls.Enable();
+        playerControls.Gameplay.Movement.performed += MovementPerformed;
+        playerControls.Gameplay.Pointer.performed += PointerPerformed;
+    }
+
+    private void OnDestroy()
+    {
+        playerControls.Gameplay.Movement.performed -= MovementPerformed;
+        playerControls.Gameplay.Pointer.performed -= PointerPerformed;
+        playerControls.Disable();
+    }
+
+    private void MovementPerformed(InputAction.CallbackContext ctx)
+    {
+        movementInput = ctx.ReadValue<Vector2>();
+    }
+
+    private void PointerPerformed(InputAction.CallbackContext ctx)
+    {
+        pointerInput = ctx.ReadValue<Vector2>();
+    }
+}
